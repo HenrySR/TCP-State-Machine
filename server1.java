c61ce3ba690791c8e9a040f063f857850a9b44cb
Mollie Shichman <mfshichman@email.wm.edu>
2020-04-13 15:11:07 -0400

    figuring out where port # lost

--- server1.java
+++ server1.java
@@ -11,8 +11,9 @@ public class server1 {
 
     try{
       TCPStart.start();
+      System.out.println("arg 0: " + Integer.parseInt(argv[0]));
       ServerSocket sock = new ServerSocket(Integer.parseInt(argv[0]));
-      System.out.println("gonna try the old sock.accept()");
+      System.out.println("port was set to " + sock.getLocalPort());
       Socket connSock = sock.accept();
 
       System.out.println("got socket "+connSock);
